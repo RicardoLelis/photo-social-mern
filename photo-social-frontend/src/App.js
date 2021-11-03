419fe387502870a5ac0393e38dd0cfe564dd49d6
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-03 22:55:51 +0000

    Setup frontend folder with npx create app. Added Header.

---
+++ photo-social-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src="logo192.png" alt="Header" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ photo-social-frontend/src/firebase.js
@@ -0,0 +1,10 @@
+import config from './config'
+
+const firebaseConfig = {
+    apiKey: config.firebaseConfig.apiKey,
+    authDomain: config.firebaseConfig.authDomain,
+    projectId: config.firebaseConfig.projectId,
+    storageBucket: config.firebaseConfig.storageBucket,
+    messagingSenderId: config.firebaseConfig.messagingSenderId,
+    appId: config.firebaseConfig.appId
+};
