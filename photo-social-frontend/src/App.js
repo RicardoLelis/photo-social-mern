9a835e143c31e62e9cf0c6555e4a3a106ca4accd
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-04 22:53:08 +0000

    Added Frontend Post component and styles.

--- photo-social-frontend/src/App.js
+++ photo-social-frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
+import Post from './components/Post'
+
 function App() {
   return (
     <div className="app">
       <div className="app__header">
         <img className="app__headerImage" src="logo192.png" alt="Header" />
       </div>
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 }
