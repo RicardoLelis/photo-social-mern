8fdf67b1eb04c63dc67ee6aab55f8532ae006f72
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-06 21:35:36 +0000

    Added site dynamics through props passing between components.

--- photo-social-frontend/src/App.js
+++ photo-social-frontend/src/App.js
@@ -1,16 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Post from './components/Post'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "TWD",
+      caption: "&#x1F525;Build a Messaging app with MERN Stack&#x1F525;",
+      imageUrl: "https://www.techlifediary.com/wp-content/uploads/2020/06/react-js.png"
+    },
+    {
+      username: "rlelis",
+      caption: "Such a beautifull world",
+      imageUrl: "https://quotefancy.com/media/wallpaper/3840x2160/126631-Charles-Dickens-Quote-And-a-beautiful-world-you-live-in-when-it-is.jpg"
+    }
+  ])
   return (
     <div className="app">
       <div className="app__header">
         <img className="app__headerImage" src="logo192.png" alt="Header" />
       </div>
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(post => (
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+
+      ))}
     </div>
   );
 }
--- photo-social-frontend/src/components/Post.js
+++ photo-social-frontend/src/components/Post.js
@@ -3,19 +3,19 @@ import './Post.css'
 
 import { Avatar } from '@material-ui/core'
 
-const Post = () => {
+const Post = ({ username, caption, imageUrl }) => {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar
                     className="post__avatar"
-                    alt="TWD"
+                    alt={username}
                     src="/static/images/avatar1.jpg"
                 />
-                <h3>TWD</h3>
+                <h3>{username}</h3>
             </div>
-            <img className="post__image" src="https://www.techlifediary.com/wp-content/uploads/2020/06/react-js.png" alt="React" />
-            <h4 className="post__text"><strong>thewebdev</strong>&#x1F525;Build a Messaging app with MERN (MongoDB, Express, React JS, Node JS) &#x1F525;</h4>
+            <img className="post__image" src={imageUrl} alt="React" />
+            <h4 className="post__text"><strong>{username}</strong>{caption}</h4>
 
         </div>
     )
