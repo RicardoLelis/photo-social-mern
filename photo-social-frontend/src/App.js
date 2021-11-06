1f680ed89df841c3cbb3b7e7ebaaa123576fe5c1
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-06 22:31:31 +0000

    Added login Modal component.

--- photo-social-frontend/src/App.js
+++ photo-social-frontend/src/App.js
@@ -3,7 +3,38 @@ import './App.css';
 
 import Post from './components/Post'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
+
 function App() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = useState(false)
   const [posts, setPosts] = useState([
     {
       username: "TWD",
@@ -16,11 +47,42 @@ function App() {
       imageUrl: "https://quotefancy.com/media/wallpaper/3840x2160/126631-Charles-Dickens-Quote-And-a-beautiful-world-you-live-in-when-it-is.jpg"
     }
   ])
+
+  const signUp = e => {
+    e.preventDefault()
+  }
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img className="app__headerImage" src="logo192.png" alt="Header" />
+            </center>
+            <Input placeholder="username"
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <Input placeholder="email"
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input placeholder="password"
+              type="text"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app__header">
         <img className="app__headerImage" src="logo192.png" alt="Header" />
       </div>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       {posts.map(post => (
         <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
 
