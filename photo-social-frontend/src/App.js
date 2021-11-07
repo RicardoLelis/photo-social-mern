0c6aa526193632ae37e70c058d2a4e05e84b212f
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-07 23:15:23 +0000

    Added header styles and display grid to center page content. Fix auth functionality for firebase.

--- photo-social-frontend/src/App.js
+++ photo-social-frontend/src/App.js
@@ -123,13 +123,13 @@ function App() {
       </Modal>
       <div className="app__header">
         <img className="app__headerImage" src="logo192.png" alt="Header" />
+        {user ? <Button onClick={() => auth.signOut()}>Logout</Button> : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
-      {user ? <Button onClick={() => auth.signOut()}>Logout</Button> : (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-      )}
       <div className="app__posts">
         {posts.map(post => (
           <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
