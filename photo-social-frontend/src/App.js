0c2995dad796d957c92941f4f2d825239606d04e
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-11-07 13:15:05 +0000

    Added signIn and signUp function using firebase utilities. Added  first user to the firebase  DB.

--- photo-social-frontend/src/App.js
+++ photo-social-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Post from './components/Post'
+import { auth } from './firebase'
 
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
@@ -35,6 +36,8 @@ function App() {
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = useState(false)
+  const [openSignIn, setOpenSignIn] = useState(false)
+  const [user, setUser] = useState(null)
   const [posts, setPosts] = useState([
     {
       username: "TWD",
@@ -48,8 +51,34 @@ function App() {
     }
   ])
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log(authUser)
+        setUser(authUser)
+      } else {
+        setUser(null)
+      }
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [user, username])
+
   const signUp = e => {
     e.preventDefault()
+    auth.createUserWithEmailAndPassword(email, password)
+      .then(authUser => authUser.user.updateProfile({ displayName: username }))
+      .catch(error => alert(error.message))
+    setOpen(false)
+  }
+
+  const signIn = e => {
+    e.preventDefault()
+    auth.signInWithEmailAndPassword(email, password)
+      .catch(error => alert(error.message))
+    setOpenSignIn(false)
+
   }
 
   return (
@@ -79,10 +108,33 @@ function App() {
           </form>
         </div>
       </Modal>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img className="app__headerImage" src="logo192.png" alt="Header" />
+            </center>
+            <Input placeholder="email" type="text" value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input placeholder="password" type="password" value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app__header">
         <img className="app__headerImage" src="logo192.png" alt="Header" />
       </div>
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      {user ? <Button onClick={() => auth.signOut()}>Logout</Button>
+        : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
+
       {posts.map(post => (
         <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
 
--- photo-social-frontend/src/firebase.js
+++ photo-social-frontend/src/firebase.js
@@ -1,3 +1,9 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';        // for authentication
+import 'firebase/compat/storage';     // for storage
+import 'firebase/compat/database';    // for realtime database
+import 'firebase/compat/firestore';   // for cloud firestore
+
 import config from './config'
 
 const firebaseConfig = {
@@ -8,3 +14,9 @@ const firebaseConfig = {
     messagingSenderId: config.firebaseConfig.messagingSenderId,
     appId: config.firebaseConfig.appId
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+export { db, auth, storage }
